8341e7041ff707b3678880fdff8c2855af3d6571
Nattapon Nimakul <totizy@gmail.com>
2019-05-28 03:40:02 +0700

    Add files via upload

---
+++ SimpleVoting.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+// specifies what version of compiler this code will be compiled with
+
+contract Voting {
+  /* the mapping field below is equivalent to an associative array or hash.
+  */
+
+  mapping (string => uint256) votesReceived;
+
+  /* Solidity doesn't let you pass in an array of strings in the constructor (yet).
+  We will use an array of bytes32 instead to store the list of candidates
+  */
+
+  string[] public candidateList;
+
+  /* This is the constructor which will be called once and only once - when you
+  deploy the contract to the blockchain. When we deploy the contract,
+  we will pass an array of candidates who will be contesting in the election
+  */
+  constructor(string[] memory candidateNames) public {
+    candidateList = candidateNames;
+  }
+
+  // This function returns the total votes a candidate has received so far
+  function totalVotesFor(string memory candidate) public view returns (uint256) {
+    return votesReceived[candidate];
+  }
+
+  // This function increments the vote count for the specified candidate. This
+  // is equivalent to casting a vote
+  function voteForCandidate(string memory candidate) public {
+    votesReceived[candidate] += 1;
+  }
+}
