16db6bd308127351c6480238c2835fc6609856ce
Nattapon Nimakul <totizy@gmail.com>
2020-01-25 12:25:35 +0700

    add generate public key example

---
+++ Nodejs/NodeWallet/generatePublicKeyAddress.js
@@ -0,0 +1,26 @@
+const util = require('ethereumjs-util');   
+const Buffer = require('safe-buffer').Buffer
+const secp256k1 = require('secp256k1')
+
+async function main() {
+  const account = {
+    // "address":"0x55509eC248c859e15293189548a8b79E2306e0CD",
+    "privateKey":"4f361e3b2c678f9f8418d530b116fb7d28d143a86f7cdd8dfd8c80aeaf936b0a"
+  }
+
+  const privateKey = Buffer.from(account.privateKey, 'hex')
+  const publicKey = Buffer.from(secp256k1.publicKeyCreate(privateKey, false).slice(1), 'hex').toString('hex')
+  const address = util.publicToAddress('0x' + publicKey).toString('hex')
+
+  console.log('privateKey:', privateKey)
+  console.log('publicKey:', publicKey)
+  console.log('address:', address)
+
+  return true
+}
+
+
+(async () => {
+  const result = await main()
+  console.log('result', result)
+})()
