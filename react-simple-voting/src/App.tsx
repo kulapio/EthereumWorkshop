bc1704d4cdee2a07ac397e193f2a48e39efccf31
Aikdanai Sidhikosol <aikdanai.s@gmail.com>
2020-08-30 14:56:13 +0700

    ✨ account info

--- react-simple-voting/src/App.tsx
+++ react-simple-voting/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+
+import { useWeb3 } from './hooks/useWeb3'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
-import { useWeb3 } from './hooks/useWeb3'
+import { Account } from './components/Account'
 
 export const App = () => {
   useWeb3()
   return (
     <div>
       <Header />
+      <Account />
       <Footer />
     </div>
   )
