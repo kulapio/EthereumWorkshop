7c1215600564cb6826a45f1f282eb41b8efd39d5
s111s <31947898+s111s@users.noreply.github.com>
2020-10-25 14:53:09 +0700

    add redeem function
    add redeem function for redeem cEther

--- Smart contract/CompoundWallet.sol
+++ Smart contract/CompoundWallet.sol
@@ -1,48 +1,48 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.6.0;
-
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.1.0/contracts/access/Ownable.sol";
-
 // CETH Interface, refer to  https://compound.finance/developers
 interface CEtherInterface {
     function mint() external payable;
     function totalSupply() external view returns(uint256);
     function balanceOf(address owner) external view returns (uint256);
     function transfer(address dst, uint256 amount) external returns (bool);
+    function redeem(uint256 redeemTokens) external;
 }
-
 // Personal Compound Wallet, capability of mint and forward CEther
 contract CompoundWallet is Ownable {
     // Address of CETH token (ERC20)
     address public cEtherAddress = 0xf92FbE0D3C0dcDAE407923b2Ac17eC223b1084E4;
-
     // Personal mobile wallet address
     address public mobileWalletAddress = 0x02621e31998B15a93B39aA643a60396dEEE8bEd2;
-
     // Fetch CETH total supply
     function getCEtherSupply() public view returns(uint256) {
         CEtherInterface cEther = CEtherInterface(cEtherAddress);
         return cEther.totalSupply();
     }
-
     // Mint CETH upon given ETH
     function deposit() public payable onlyOwner {
         CEtherInterface cEther = CEtherInterface(cEtherAddress);
         cEther.mint.value(msg.value)();
     }
-
+    function redeem() public onlyOwner {
+        CEtherInterface cEther = CEtherInterface(cEtherAddress);
+        uint256 balance = cEther.balanceOf(address(this));
+        cEther.redeem(balance);
+        msg.sender.transfer(address(this).balance);
+    }
+    fallback () external payable {
+        // revert(); // throw reverts state to before call
+    }
     // Transfer minted CETH from this contract to caller
     function withDrawCEther() public onlyOwner {
         CEtherInterface cEther = CEtherInterface(cEtherAddress);
         uint256 balanceToTransfer = cEther.balanceOf(address(this));
         cEther.transfer(msg.sender, balanceToTransfer);
     }
-
     // Mint CETH and transfer to target wallet
     function mintCEtherToMobile() public payable {
         deposit();
-
         CEtherInterface cEther = CEtherInterface(cEtherAddress);
         uint256 balanceToTransfer = cEther.balanceOf(address(this));
         cEther.transfer(mobileWalletAddress, balanceToTransfer);
